219e84af8cacbb3e3096d69cec1e42fd6b5a7c66
LUIS CARLOS MARTINS ARRUDA JUNIOR <36969510+lcmajr@users.noreply.github.com>
2025-06-03 09:28:13 -0300

    Update autoclick.js

--- autoclick.js
+++ autoclick.js
@@ -4,43 +4,38 @@
 
 function clicarBotaoPlanFarms() {
 
-setTimeout(() => {
-    const eventoEnter = new KeyboardEvent('keydown', {
-    key: 'Enter',
-    keyCode: 13,
-    which: 13,
-    bubbles: true,
-    cancelable: true
-    });
+const eventoEnter = new KeyboardEvent('keydown', {
+key: 'Enter',
+keyCode: 13,
+which: 13,
+bubbles: true,
+cancelable: true
+});
 
-    const tabela = document.querySelector("#content_value > div.vis.farmGodContent > table");
-    const linhas = tabela?.querySelectorAll("tbody tr");
+const tabela = document.querySelector("#content_value > div.vis.farmGodContent > table");
+const linhas = tabela?.querySelectorAll("tbody tr");
 
-    if (linhas) {
-    console.log("Total de linhas na tabela:", linhas.length);
-    for (let i = 0; i < linhas.length - 2; i++) {
-        setTimeout(() => {
-        document.dispatchEvent(eventoEnter);
-        console.log('[Fase 2] Tecla ENTER pressionada após clique em "Plan farms".');
-        console.log("Índice:", i);
+if (linhas) {
+console.log("Total de linhas na tabela:", linhas.length);
+for (let i = 0; i < linhas.length - 2; i++) {
+    setTimeout(() => {
+    document.dispatchEvent(eventoEnter);
+    console.log('[Fase 2] Tecla ENTER pressionada após clique em "Plan farms".');
+    console.log("Índice:", i);
 
-        // Se for o último, marcamos como finalizado
-        if (i === linhas.length - 3) {
-            console.log("Execução finalizada.");
+    // Se for o último, marcamos como finalizado
+    if (i === linhas.length - 3) {
+        console.log("Execução finalizada.");
 
-        }
-        }, i * 1000);
     }
-    } else {
-    console.warn("Tabela ou linhas não encontradas.");
-    }
-}, 3000);
+    }, i * 1000);
+}
+} else {
+console.warn("Tabela ou linhas não encontradas.");
+}
+
  
 }
 
-function executarFarmGod() {
-    clicarBotaoPlanFarms();
-    
-}
 
-executarFarmGod();
+clicarBotaoPlanFarms();
